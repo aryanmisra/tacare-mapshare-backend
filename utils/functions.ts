8479ddb3e2ab35cc7b49bdcb352770322fb8c17c
Aryan Misra <aryanmisra4@gmail.com>
2021-08-12 21:04:31 -0400

    uid

--- utils/functions.ts
+++ utils/functions.ts
@@ -4,6 +4,6 @@ const algorithm = "aes-256-cbc";
 
 export const generateCode = (len: number) => [...Array(len)].map(() => Math.floor(Math.random() * 16).toString(16)).join("");
 
-export const generateAccessToken = (uid: string) => {
-  return jwt.sign({ uid: uid }, config.JWT_KEY, { expiresIn: "24h" });
+export const generateAccessToken = (uid: any) => {
+  return jwt.sign(uid, config.JWT_KEY, {expiresIn: "24h"});
 };
--- utils/middleware.ts
+++ utils/middleware.ts
@@ -6,14 +6,6 @@ import User from "../models/user";
 const dsConfig = require("../dsconfig.js");
 const DSAuthCodeGrant = require("../lib/DSAuthCodeGrant");
 
-// export const updateProfileSchema = (req, res, next) => {
-//   const schema = Joi.object({
-//     email: Joi.string().email().required(),
-//     firstName: Joi.string().required(),
-
-//   });
-//   validateRequest(req, next, schema);
-// };
 
 export const registerSchema = (req, res, next) => {
   const schema = Joi.object({
@@ -40,7 +32,7 @@ const validateRequest = (req, next, schema) => {
     allowUnknown: true, // ignore unknown props
     stripUnknown: true, // remove unknown props
   };
-  const { error, value } = schema.validate(req.body, options);
+  const {error, value} = schema.validate(req.body, options);
   if (error) {
     next(createError(400, `Validation Error`));
   } else {
@@ -60,7 +52,7 @@ export const requestLogger = (req, res, next) => {
 };
 
 export const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+  res.status(404).send({error: "unknown endpoint"});
 };
 
 export const errorHandler = (error, req, res, next) => {
